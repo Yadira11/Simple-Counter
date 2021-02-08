e64bb6c9fe76c2eadb427d42831c103822467152
Yadira11 <chavarriayadira85@gmail.com>
2021-02-08 23:06:21 +0000

    fixed- counter

---
+++ src/js/component/icons.js
@@ -0,0 +1,6 @@
+import fontawesome from "@fortawesome/fontawesome";
+import faClock from "@fortawesome-free-regular/facheck";
+fontawesome.config = {
+	autoReplaceSvg: "nest"
+};
+fontawesome.library.add(faClock);
